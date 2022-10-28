287c00493cff95141fa464a3ccc55e3f416339f0
Peter Chisom <peterchisom69@gmail.com>
2022-10-28 23:48:13 +0100

    added/- images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import user from "./assets/user.png";
+import slack from "./assets/svgs/slack.svg";
+import github from "./assets/svgs/github.svg";
+import { info } from "./data";
 
 const App = () => {
   return (
-    <div className="font-sans">
-      
+    <div className="font-sans mx-auto py-16  h-screen">
+      <main className="px-8 flex flex-col justify-center items-center ">
+        <div className="mb-14 flex flex-col justify-center items-center ">
+          <img
+            className="object-cover bg-no-repeat h-[88px] w-[88px] rounded-full mb-6"
+            src={user}
+            alt="Peter"
+          />
+          <p className="font-bold">Peter Jackson</p>
+        </div>
+      </main>
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const info = [
+  {
+    username: "lucy",
+  },
+  {
+    name: "twitter",
+    twitter: "twitter.com",
+  },
+  {
+    name: "github",
+    github: "github.com",
+  },
+  {
+    name: "linkedin",
+    linkedin: "linkedin.com",
+  },
+  {
+    name: "linkedin",
+    linkedin: "linkedin.com",
+  },
+];
