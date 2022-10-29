30925655bf9a119dc5ac7c77143bb5f919258745
Peter Chisom <peterchisom69@gmail.com>
2022-10-29 04:46:05 +0100

    changed/- profile_img link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
           <img
             id="profile_img"
             className="object-cover bg-no-repeat h-[88px] w-[88px] rounded-full mb-6"
-            src="https://pbs.twimg.com/profile_images/1548036397503721473/EiDxIeTt_400x400.jpg"
+            src="https://pbs.twimg.com/profile_images/1586201750695968770/KdBc6YZQ_400x400.jpg"
             alt="chiisom"
           />
           <p id="twitter" className="font-bold text-[18px] md:text-xl">
