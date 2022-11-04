e6e81f24ef109d58e7ebaf4f4bf4b1282d8d0ecc
Peter Chisom <peterchisom69@gmail.com>
2022-11-04 22:04:22 +0100

    chore & style: designed contat headin gand created input component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div className='grid place-items-center'>
-            <h2>Contact Page</h2>
-        </div>
-    );
-}
+  return (
+    <div className="px-8">
+      <h2 className="font-semibold text-4xl mb-5">Contact Me</h2>
+      <p className="text-xl mb-12">
+        Hi there, contact me to ask me about anything you have in mind.
+      </p>
+      <form>
+        
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
