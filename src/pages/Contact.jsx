56cf5e227e4e5568753879e59a31fd318f151f30
Peter Chisom <peterchisom69@gmail.com>
2022-11-05 09:22:31 +0100

    feat: added form validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Contact = () => {
   const name = "Peter";
-  
 
+  const initValues = { firstname: "", lastname: "", email: "", message: "" };
+  const [formValues, setFormValues] = useState(initValues);
+  const [formError, setFormError] = useState({});
+
+  const handleChange = (e) => {
+    const { firstname, value } = e.target;
+    setFormValues({ ...formValues, [firstname]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError(vaidateForm(formValues));
+  };
+
+  function vaidateForm(values) {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    const { firstname, lastname, email, message } = values;
+    if (!firstname) errors.firstname = "Please enter your firstname";
+    if (!lastname) errors.lastname = "Please enter your lastname";
+    if (!email) errors.email = "please enter a valid email";
+    if (!message) errors.message = "Please enter a message";
+    return errors;
+  }
   return (
     <div className="relative px-4 md:max-w-[720px] h-[616px] mx-auto mt-16 md:mt-[96px] mb-[280px] md:mb-[188px]">
       <h2 className="font-semibold text-4xl mb-5 text-zuri-gray-900">
@@ -12,14 +35,17 @@ const Contact = () => {
       <p className="text-lg md:text-xl mb-12 text-zuri-gray-600">
         Hi there, contact me to ask me about anything you have in mind.
       </p>
-      <form className="grid gap-y-6 gap-x-6">
+      <form className="grid gap-y-6 gap-x-6" onSubmit={handleSubmit}>
         <label className="label-style">
           First name
           <input
             name="firstname"
+            // ref={firstname}
             id="first_name"
+            value={initValues.firstname}
             className="input-style md:col-span-6"
             placeholder="Enter your first name"
+            onChange={handleChange}
           />
         </label>
         <label className="label-style">
@@ -27,8 +53,10 @@ const Contact = () => {
           <input
             name="lastname"
             id="last_name"
+            value={initValues.lastname}
             className="input-style md:col-span-6"
             placeholder="Enter your last name"
+            onChange={handleChange}
           />
         </label>
         <label className="label-long-style">
@@ -36,16 +64,20 @@ const Contact = () => {
           <input
             name="email"
             id="email"
+            value={initValues.email}
             className="input-style col-span-12"
             placeholder="yourname@email.com"
+            onChange={handleChange}
           />
         </label>
         <label className="label-long-style">
           Message
           <textarea
             id="message"
+            value={initValues.message}
             className="col-span-12 py-3 px-[14px] w-full h-[132px] input-style"
             placeholder="Send me a message and I'll reply you as soon as possible..."
+            onChange={handleChange}
           />
         </label>
         <span className="flex items-start md:items-center mb-8 col-span-12">
